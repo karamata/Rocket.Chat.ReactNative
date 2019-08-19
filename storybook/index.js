de2450e4e7c988f5c20608a626d89d5966928c0b
tran.phu <bkit07@hotmail.com>
2019-08-19 18:30:06 +0700

    before merge

--- storybook/index.js
+++ storybook/index.js
@@ -17,6 +17,6 @@ const StorybookUIRoot = getStorybookUI({});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('EdChat', () => StorybookUIRoot);
+AppRegistry.registerComponent('RocketChatRN', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
